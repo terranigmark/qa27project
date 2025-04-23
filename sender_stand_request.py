06ee17d7517f0d1ea14024d669bceb51fbfb5c5a
terranigmark <linnk99@gmail.com>
2025-04-22 18:35:53 -0700

    funcion get_logs creada en sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -6,3 +6,11 @@ def get_docs():
 
 response = get_docs()
 print(response.status_code)
+
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH)
+
+response = get_logs()
+print(response.status_code)
+print(response.headers)
+

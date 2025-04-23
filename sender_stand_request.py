5475ff00fa9ee20254742d44f13f805b27329279
terranigmark <linnk99@gmail.com>
2025-04-22 18:38:09 -0700

    funcion get_logs mejorada

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -8,9 +8,9 @@ response = get_docs()
 print(response.status_code)
 
 def get_logs():
-    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH)
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH,
+                        params={"count": 20})
 
 response = get_logs()
 print(response.status_code)
 print(response.headers)
-
